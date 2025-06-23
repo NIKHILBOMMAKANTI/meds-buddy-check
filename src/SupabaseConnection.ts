5fa99c240f856728fb0ac9cfffd44f3eb916b7b6
NIKHILBOMMAKANTI <nikhilbommakanti2001@gmail.com>
2025-06-24 01:47:11 +0530

    Changes Related to the Vercel

--- src/SupabaseConnection.ts
+++ src/SupabaseConnection.ts
@@ -1,5 +1,5 @@
 import {createClient} from '@supabase/supabase-js'
-const supabaseurl:string = import.meta.env.VITE_SUPABASEURL
+const supabaseurl:string = import.meta.env.VITE_SUPABASE_URL
 const supabaseanonkey:string = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export  const supabase = createClient(supabaseurl,supabaseanonkey);
